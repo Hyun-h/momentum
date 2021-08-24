c4271976de47855902688e64dd352f0e30a9149d
Hyun-h <69190826+Hyun-h@users.noreply.github.com>
2021-08-24 16:54:51 +0900

    THE END

--- js/weather.js
+++ js/weather.js
@@ -6,8 +6,12 @@ function onGeonOk(position) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
-    .then((date) => {
-      console.log(data.name, data.weather[0].main);
+    .then((data) => {
+      const temperature = Math.round(data.main.temp * 10) / 10;
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = `${data.sys.country} ${data.name}`;
+      weather.innerText = `${data.weather[0].main} ${temperature}℃`;
     });
 }
 function onGeonError() {
